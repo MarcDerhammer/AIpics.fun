d6ebacb70267c99c60407a8793aeee0742957505
Marc Derhammer <marc.derhammer@gmail.com>
2022-06-14 20:55:14 -0400

    inf scroll

--- client/dallephone/src/components/screens/Gallery.tsx
+++ client/dallephone/src/components/screens/Gallery.tsx
@@ -6,28 +6,55 @@ import './UserScreen.css';
 const Gallery = () => {
   const [page, setPage] = React.useState(0);
   const [photos, setPhotos] = React.useState<any[]>([]);
+  const [moreExists, setMoreExists] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+
+  let scrollTimeout:any;
+
+  const handleScroll = () => {
+    if (!moreExists) {
+      return;
+    }
+    clearInterval(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      if (window.innerHeight + document.documentElement.scrollTop + 600 <
+        document.documentElement.offsetHeight) return;
+      setPage(prevPage => prevPage + 1);
+    }, 250);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   useEffect(() => {
     (async () => {
-      const photos = await showAllPublic({
-        limit: 8,
-        offset: page * 10,
+      if (!moreExists) {
+        return;
+      }
+      setLoading(true);
+      const newPhotos = await showAllPublic({
+        limit: 11,
+        offset: page * 12,
         orderBy: 'created_at',
         ascending: false
       });
-      setPhotos(photos);
+      setLoading(false);
+      if (newPhotos.length === 0) {
+        setMoreExists(false);
+        return;
+      }
+      setPhotos(photos.concat(newPhotos));
+      handleScroll();
     }
     )();
   }, [page]);
 
   return (
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginBottom: '20px'
-        }}>
-          <h2>public gallery:</h2>
+    <div>
             <div style={{
               display: 'flex',
               flexDirection: 'row',
@@ -56,21 +83,9 @@ const Gallery = () => {
               })
           }
           </div>
-          <div>
-            {
-              page > 0 &&
-              <span className="paging" onClick={() => {
-                setPage(page - 1);
-              }
-              }>&lt;previous page</span>
-            }
-            {
-              <span className="paging" onClick={() => {
-                setPage(page + 1);
-              }
-              }>next page&gt;</span>
-            }
-          </div>
+          {
+            loading && <div>Loading...</div>
+          }
         </div>
   );
 };
